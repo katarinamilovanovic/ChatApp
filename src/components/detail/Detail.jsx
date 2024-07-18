6d00ddf858b65c1cecb95f1a51e8bec1c7e324c2
Katarina Milovanovic <k.milovanovic55@gmail.com>
2024-07-18 12:47:25 +0200

    Dizajn Details komponente

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -4,7 +4,78 @@ import "./detail.css"
 const Detail = () => {
     return (
       <div className='detail'>
-        Detail
+        <div className="user">
+            <img src="./avatar.png" alt="" />
+            <h2>Jane Doe</h2>
+            <p>Lorem ipsum dolor sit amet consec</p>
+        </div>
+        <div className="info">
+            <div className="option">
+                <div className="title">
+                    <span>Chat settings</span>
+                    <img src="./arrowUp.png" alt="" />
+                </div>
+            </div>
+
+            <div className="option">
+                <div className="title">
+                    <span>Privacy & help</span>
+                    <img src="./arrowUp.png" alt="" />
+                </div>
+            </div>
+
+            <div className="option">
+                <div className="title">
+                    <span>Shared photos</span>
+                    <img src="./arrowDown.png" alt="" />
+                </div>
+                <div className="photos">
+                    <div className="photoItem">
+                        <div className="photoDetail">
+                            <img src="./avatar.png" alt="" />
+                            <span>photo 2020.png</span>
+                        </div>
+                        <img src="./download.png" alt="" className="icon"/>
+                    </div>
+
+                    <div className="photoItem">
+                        <div className="photoDetail">
+                            <img src="./avatar.png" alt="" />
+                            <span>photo 2020.png</span>
+                        </div>
+                        <img src="./download.png" alt="" className="icon"/>
+                    </div>
+
+                    <div className="photoItem">
+                        <div className="photoDetail">
+                            <img src="./avatar.png" alt="" />
+                            <span>photo 2020.png</span>
+                        </div>
+                        <img src="./download.png" alt="" className="icon"/>
+                    </div>
+
+                    <div className="photoItem">
+                        <div className="photoDetail">
+                            <img src="./avatar.png" alt="" />
+                            <span>photo 2020.png</span>
+                        </div>
+                        <img src="./download.png" alt="" className="icon"/>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="option">
+                <div className="title">
+                    <span>Shared Files</span>
+                    <img src="./arrowUp.png" alt="" />
+                </div>
+            </div>
+
+            <button>Block User</button>
+
+
+        </div>
       </div>
     ) 
   }
