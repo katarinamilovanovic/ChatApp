f75d261a77810688cf1daac151c24d3d8afe5798
Katarina Milovanovic <k.milovanovic55@gmail.com>
2024-07-23 13:49:30 +0200

    Slanje nove poruke u cet

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -38,8 +38,33 @@ const ChatList = () => {
     }, [currentUser.id]);
 
     const handleSelect = async (chat) => {
-        changeChat(chat.chatId, chat.user);
-    }
+
+        const userChats = chats.map((item) => {
+            const {user, ...rest} = item;
+            return rest;
+        });
+
+        const chatIndex = userChats.findIndex(
+            (item) => item.chatId === chat.chatId
+        );
+
+        userChats[chatIndex].isSeen = true;
+        
+        const userChatsRef = doc(db, "userchats", currentUser.id);
+
+        try {
+
+            await updateDoc(userChatsRef, {
+                chats: userChats,
+            });
+
+            changeChat(chat.chatId, chat.user);
+
+        } catch (err) {
+            console.log(err);
+        }
+
+    };
 
 
     return (
@@ -58,7 +83,14 @@ const ChatList = () => {
         </div>
 
         {chats.map((chat) => (
-            <div className="item" key={chat.chatId} onClick={() => handleSelect(chat)}>
+            <div 
+                className="item" 
+                key={chat.chatId} 
+                onClick={() => handleSelect(chat)}
+                style={{
+                    backgroundColor: chat?.isSeen ? "transparent" : "#5183fe"
+                }}
+            >
                 <img src={chat.user.avatar || "./avatar.png"} alt="" />
                 <div className="texts">
                     <span>{chat.user.username}</span>
